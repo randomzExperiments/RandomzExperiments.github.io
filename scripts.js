bd26d11fe5b19ac5766c0adc4c0a598c76f1e2c9
Jaka Mušič <Jaka Mui>
2017-02-08 14:09:24 +0100

    Initial

---
+++ scripts.js
@@ -0,0 +1,9 @@
+$(".button").click(function(e) {
+    $(".article").fadeIn(300).load($(e).parents(".thumb").data("source")); //tuki je ta del za klicanje
+    $(".toolbar").stop().animate({"backgroundColor":"#656760","color":"#fafafc"}, 1000);                
+});
+
+$(".button-close").click(function() {
+    $(".toolbar").stop().animate({"backgroundColor":"#fafafc","color":"#656760"}, 1300);
+    $(".article").fadeOut(400).html("");              
+});
